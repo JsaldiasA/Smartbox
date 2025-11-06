56da2c24925e96feb2a1878b2ffd427e434aba5f
unknown <hsuarez@eco3.cl>
2025-11-06 17:05:55 -0300

    sorting

--- ApiController/GetUnidad.php
+++ ApiController/GetUnidad.php
@@ -41,10 +41,17 @@ $isEstanque = ($unidadTipo_Nombre == 'Estanque7600') ? true : false;
 		}
 
 usort($UnidadesFiltradasPorTipo, function($a, $b) {
+    if ($a->get_UltimaActualizacion() == $b->get_UltimaActualizacion()) {
+        return 0;
+    }
+    return ($a->get_UltimaActualizacion() > $b->get_UltimaActualizacion()) ? -1 : 1;
+});
+
+usort($UnidadesFiltradasPorTipo, function($a, $b) {
     if ($a->get_Volumen() == $b->get_Volumen()) {
         return 0;
     }
-    return ($a->get_Volumen() < $b->get_Volumen()) ? -1 : 1;
+    return ($a->get_Volumen() > $b->get_Volumen()) ? -1 : 1;
 });
 
 
