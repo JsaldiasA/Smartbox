775f6e6f134f738a8800e3f0783ed0aecf39897a
JsaldiasA <javier.saldias.araya@gmail.com>
2025-11-10 22:56:14 +0900

    fix main 9

--- vini.php
+++ vini.php
@@ -64,13 +64,6 @@ else
 	}
 }
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
 $IP = $_SERVER['REMOTE_ADDR'];
 
 //Insertando En tabla RowData
