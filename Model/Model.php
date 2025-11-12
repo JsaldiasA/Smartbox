3c99586bf07c715d887a1f9fb1cd040e9f184dd4
hsuarezeco3 <hsuarez@eco3.cl>
2025-11-12 11:29:06 -0300

    666

--- Model/Model.php
+++ Model/Model.php
@@ -22,7 +22,7 @@ class Model {
 	function get_unidades_lastortolas()
 
 		{ 			
-			$sql = "SELECT * FROM `unidades_lastortolas` LIMIT 1000";
+			$sql = "SELECT * FROM `unidades_lastortolas` ORDER BY ID DESC LIMIT 10000";
 			$result = $this->executeSQL($sql);
 			
 			$unidades_lastortolas = [];
