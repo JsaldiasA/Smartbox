886f8e4db0cd8bf7212503d4bfc1703111196d02
hsuarezeco3 <hsuarez@eco3.cl>
2025-12-18 13:00:42 -0300

    fix_edit_unitype

--- UnidadCambiarTipo.php
+++ UnidadCambiarTipo.php
@@ -1,5 +1,4 @@
 <?php
-
 $self=$_SERVER['PHP_SELF'];
 $thispath=dirname($_SERVER['PHP_SELF']);
 $sitebasepath=$_SERVER['DOCUMENT_ROOT'];
@@ -13,14 +12,13 @@ $token= $_POST['token'];
 
 if (strcmp($token,'eco3spa')==0)
 {
-	$sql = "CALL unidadCambiarTipo('{$tag}','{$NuevoTipo}')";
+	$sql = "UPDATE `unidad` SET `id_unidadTipo` = '{$NuevoTipo}' WHERE `tag` LIKE '{$tag}'";
 	$result = $model->executeSQL($sql);
 
-	echo "Cambio de nombre hecho correctamente Nuevo Tipo:".$NuevoTipo.$tag;	
+	echo "Cambio de tipo de unidad hecho correctamente.";
 }
 else
 {
-	echo "password Incorrecta";
+	echo "Contraseña incorrecta.";
 }
-
 ?>
