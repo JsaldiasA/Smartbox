c078ae1a545e14213fbaed1cb7bd220aa1249b96
hsuarezeco3 <hsuarez@eco3.cl>
2025-12-18 15:26:44 -0300

    343434

--- UnidadVer.php
+++ UnidadVer.php
@@ -345,15 +345,13 @@ function FunctionCambiarVolMax(unidad) {
 	
 	?>
 
-
-
 <div class="row">
 	<div class="col">
 <div class="accordion" id="accordionExample">
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-        Editar
+        Edición
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
