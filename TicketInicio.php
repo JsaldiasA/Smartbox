8d786745504ec3a70b8d9cf01f9d5db567bd5d02
hsuarezeco3 <hsuarez@eco3.cl>
2025-12-23 14:12:11 -0300

    333

--- TicketInicio.php
+++ TicketInicio.php
@@ -5,12 +5,7 @@
 require_once 'views/head.php';	
 require_once 'views/navbar.php';
 
-'<style>
-        body
-        {
-            background-color: #222222;
-        }
-</style>';
+echo '<style>body{background-color: #222222;}</style>';
 
 echo '<div class="container">';
 echo '<div class="row">';
