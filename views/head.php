25fb61e09949f06ccbee024c42efcb5977900587
hsuarezeco3 <hsuarez@eco3.cl>
2025-12-23 17:32:27 -0300

    333

--- views/head.php
+++ views/head.php
@@ -1,6 +1,7 @@
 <?php
 
 echo'<head>
+
 	<!-- Meta tags -->
 	<title>Eco3</title>
 	<link rel="shortcut icon" href="/favicon/favicon.jpg"/> 
@@ -10,13 +11,13 @@ echo'<head>
 
 	<!-- stylesheets  -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.css">
-<script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
+	<link rel="stylesheet" href="https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.css">
+	<script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
 
 	<!-- scripts -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 	<script src="https://cdn.datatables.net/2.3.4/js/dataTables.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-</head>'
+	</head>'
 ?>
