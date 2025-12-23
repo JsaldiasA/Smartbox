5987538c411e04ff6dac123abd7878351f0aa2cd
hsuarezeco3 <hsuarez@eco3.cl>
2025-12-23 12:10:49 -0300

    yuyuyu

--- TicketInicio.php
+++ TicketInicio.php
@@ -8,7 +8,7 @@ require_once 'views/navbar.php';
 echo '<div class="container">';
 echo '<br><h1><b>Tickets</b></h1><br>';
 echo '<div class="subContainer">';
-echo '<button onclick = a href="../TicketForm/TicketForm.php" class="btn btn-primary"> + </button> <b> Ingresar ticket.<b>';
+echo '<button onclick = "../TicketForm/TicketForm.php" class="btn btn-primary"> + </button> <b> Ingresar ticket.<b>';
 echo '</div>';
 echo '<table class="table"><tbody>';
 echo "<tr><td><b>Página para visualizar los tickets.</b></td></tr>";
