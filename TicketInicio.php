b81bd65935b627b409ea93e4cc70a009ccaeec88
hsuarezeco3 <hsuarez@eco3.cl>
2025-12-23 12:47:40 -0300

    tk5

--- TicketInicio.php
+++ TicketInicio.php
@@ -8,25 +8,15 @@ require_once 'views/navbar.php';
 echo '<div class="container">';
 echo '<div class="row">';
 echo '<div class="col">';
-echo '<br><h1><b>Tickets</b></h1>';
+echo '<br><br><h1><b>Tickets</b></h1>';
 echo '</div>';
 echo '<div class="col">';
-echo '<tr><td><b>Columna 2.</b></td></tr>';
+echo '<tr><td><b></b></td></tr>';
 echo '</div>';
 echo '<div class="col">';
 echo '<br><br><p class="text-end"><b>Ingresar nuevo ticket. <b><a href="../TicketForm/TicketForm.php" class="btn btn-primary" role="button"> + </a>';
 echo '</div>';
 echo '</div>';
-echo '</div>';
-
-// echo '<div class="container">';
-// echo '<br><h1><b>Tickets</b></h1><br>';
-// echo '<div class="subContainer">';
-
-echo '</div>';
-echo '<table class="table"><tbody>';
-echo "<tr><td><b>Página para visualizar los tickets.</b></td></tr>";
-echo '</tbody></table>';
 echo '</div>';
 
 ?>
