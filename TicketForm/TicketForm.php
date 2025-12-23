64877a077306818c2d09d36389515923a3c2ef8a
hsuarezeco3 <hsuarez@eco3.cl>
2025-12-23 13:27:38 -0300

    tk6

--- TicketForm/TicketForm.php
+++ TicketForm/TicketForm.php
@@ -11,70 +11,84 @@ require_once $sitebasepath."/views/navbar.php";
 require_once $sitebasepath."/Model/model.php";
 
 ?>
-
 <script>
 
-	function FunctionNuevoCheckListPost() {
-  let text = "¿Está seguro de enviar el CheckList?";
-  if (confirm(text) == true) { 
-	  
-	let pattern = /(^\d+\.\d+$)|(^\d+$)/; 
-	var URL = "NuevoCheckListPost.php"; 
-	var Respuesta;
-	  
-	var Observaciones= document.getElementById("Observaciones").value;
-	if(Observaciones == "" ){ return alert("Observaciones no puede estar vacío, coloque alguna observación. Si no tiene coloque OK");}  
+function FunctionNuevoTicketPost()
+{
+	let text = "¿Está seguro de publicar el ticket?";
+	if (confirm(text) == true)
+		{
+			let pattern = /(^\d+\.\d+$)|(^\d+$)/;
+			var URL = "NuevoTicketPost.php";
+			var Respuesta;
 
-	var TecnicoResponsable= document.getElementById("TecnicoResponsable").value;
-	if(TecnicoResponsable == "" ){ return alert("Técnico responsable no puede estar vacío, coloque su nombre.");}
-	  
-  
-	 
-	$.ajax({
-            url:URL,    //the page containing php script
-            type: "post",    //request 
-			dataType: 'text',
-			data: {
-			Observaciones: Observaciones,
-			TecnicoResponsable: TecnicoResponsable, 
-        	},
-		    success: function(result){alert(result)}    
-		  });
+	  		var Ubicacion= document.getElementById("Ubicacion").value;
+			if (Ubicacion == "")
+				{
+					return alert ("Debe especificar un dispositivo o plataforma.");
+				}
 
-	window.location.href = "https://smartbox.eco3.cl/";
-  	} else {
-    	alert("La operación se ha cancelado.");
-  	}
-	}
-	
+			var Descripcion= document.getElementById("Descripcion").value;
+			if (Descripcion == "" )
+				{
+					return alert ("Debe explicar de que se trata el problema.");
+				}
+
+			var Usuario= document.getElementById("Usuario").value;
+			if (Usuario == "" )
+				{
+					return alert ("Debe escribir su nombre.");
+				}
+
+			$.ajax({
+            url:URL,
+            type:"post",
+			dataType:'text',
+			data:
+				{
+					Nombre: Nombre,
+					Ubicacion: Ubicacion,
+					Descripcion: Descripcion,
+					Usuario: Usuario,
+					FechaApertura: FechaApertura,
+					Id_TicketStatus: Id_TicketStatus,
+        		},
+			success: function(result)
+				{
+					alert (result)
+				}
+			});
+			window.location.href = "https://smartbox.eco3.cl/";
+  		}
+	else
+		{
+    		alert ("La operación se ha cancelado.");
+  		}
+}
+
 </script>
-
-	<!-- //Navigation -->
-<!-- SCRIPTS PARA editar panel -->
-
-
-<div class="container">
-	<div class="row">
-	<div class="col">	
-	<br>
-	<h1 class="display-2"><b>Checklist para </b><?php echo $UnidadTag;?></h1>
-	<br>
-	</div>
-	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col">	
+				<br><h1 class="display-2"><b>Nuevo formulario</b><?php echo $UnidadTag;?></h1><br>
+			</div>
+		</div>
 	<div class="row">
 	<div class="col border">
 	<table class="table">
-	<tbody>	
-
-	<tr><td><b>Observaciones:</b></td><td><input type="text" class="form-control" id="Observaciones" placeholder="Si no tiene comentarios, coloque OK."></td><td></td></tr>
-	<tr><td><b>Técnico responsable:</b></td><td><input type="text" class="form-control" id="TecnicoResponsable" placeholder="Nombre"></td><td></td></tr>
-			</tbody></table>
+		<tbody>
+			<tr><td><b>1:</b></td><td><input type="text" class="form-control" id="Título:" placeholder=""></td><td></td></tr>
+			<tr><td><b>2:</b></td><td><input type="text" class="form-control" id="Ubicación:" placeholder="Dispositivo y/o plataforma."></td><td></td></tr>
+			<tr><td><b>3:</b></td><td><input type="text" class="form-control" id="Descripción:" placeholder=""></td><td></td></tr>
+			<tr><td><b>4:</b></td><td><input type="text" class="form-control" id="Usuario:" placeholder=""></td><td></td></tr>
+		</tbody>
+	</table>
+	</div>
+	</div>
+	<div class="row">
+		<div class="col" >
+			<button type="button" class="btn btn-primary btn-lg" onclick="FunctionNuevoTicketPost()">Enviar ticket</button>
 		</div>
 	</div>
-				<div class="row">
-				<div class="col" >
-	<button type="button" class="btn btn-primary btn-lg" onclick="FunctionNuevoCheckListPost()">Enviar Ticket</button>
-	</div>
-</div>
 </body>
 </html>
