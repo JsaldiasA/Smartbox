69f8f6dcf0220b53b7f92bb67e6df78aa9e01302
hsuarezeco3 <hsuarez@eco3.cl>
2025-12-24 09:23:04 -0300

    444

--- UnidadVerRowData.php
+++ UnidadVerRowData.php
@@ -1,37 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<!-- Meta tags -->
-	<title>Eco3</title>
-	<link rel="shortcut icon" href="favicon/favicon.jpg"/> 
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
 
-	
-	<!-- stylesheets  -->
-	<!--<link rel="stylesheet" href="css/bootstrap.min.css">-->
-	<link rel="stylesheet" href="css/easy-responsive-tabs.css">
-	<link rel="stylesheet" href="css/font-awesome.css">
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	
-	<!-- scripts -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-	<!--<script src="js/bootstrap.min.js"></script>-->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-	<script src="js/easyResponsiveTabs.js"></script>
-	<script src="js/numscroller-1.0.js"></script>
-	<div class="left"></div>
-	<!-- google fonts -->
-	<link rel="stylesheet" href="css/fonts.googleapis.OpenSans.css">
-	<link rel="stylesheet" href="css/fonts.googleapis.Raleway.css">
+require_once 'views/head.php';	
+require_once 'views/navbar.php';
 
-</head>
+?>
 <body>
-
- <style>
+<style>
 
 .container {
 	padding:0px 0px 20px 0px;
