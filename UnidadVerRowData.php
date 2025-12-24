9dc5c32d23f5255c1a09af5184faa6f582bf433c
hsuarezeco3 <hsuarez@eco3.cl>
2025-12-24 09:27:59 -0300

    333

--- UnidadVerRowData.php
+++ UnidadVerRowData.php
@@ -6,91 +6,6 @@ require_once 'views/head.php';
 require_once 'views/navbar.php';
 
 ?>
-<body>
-<style>
-
-.container {
-	padding:0px 0px 20px 0px;
-	width: 100%;
-}
-	 
-.subContainer {
-	padding:0px 0px 20px 0px;
-	width: 100%;
-}	 
-	
-	 
-.topnav {
-  overflow: hidden;
-  background-color: #17202A ;
-  vertical-align: middle;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  text-align: center;
-  padding: 14px 16px;
-  font-size: 17px;
-  color:#ABB2B9  ;
-  text-shadow: 2px 2px 4px #000000;
-  margin-top: 10px;
-}
-
-.topnav a:hover {
-  color: white;
-}
-
-.topnav a.active {
-  background-color: #04AA6D;
-  color: white;
-}
-
-.topnav .icon {
-  display: none;
-}
-
-@media screen and (max-width: 600px) {
-  .topnav a:not(:first-child) {display: none;}
-  .topnav a.icon {
-    float: left;
-    display: block;
-  }
-}
-
-@media screen and (max-width: 600px) {
-  .topnav.responsive {position: relative;}
-  .topnav.responsive .icon {
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-  .topnav.responsive a {
-    float: none;
-    display: block;
-    text-align: left;
-	clear: right;
-	
-  }
-	 }
-	 .topnav-right {
-  float: right;
-}
-</style>
-<div class="topnav" id="myTopnav">
-  <a style="display: flex;justify-content: center;padding: 10px 10px;margin-top: 0px;">
-  <div><img src="images/Logocortado.png"  style="margin-left: 0px;width:55px;height:auto;"></div>
-  <div><img src="images/Logo letra.png"  style="margin-left: -5px;margin-right: 15px;margin-top: 5px;width:60px;height:auto;"></div></a>
-  <div id="RmyTopnav" class="topnav-right">
-  <a  href="index.php" class="active">Home</a>
-  <a  href="OldVersion.php" class="active">OldVersion</a> 	
-   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
-  </div>  
-  </div>
-  </div>
-
 
 <script>
 function myFunction() {
@@ -360,7 +275,4 @@ $(document).ready(function () {
 $conn->close();
 ?>
 
-
-
-</body>
 </html>
