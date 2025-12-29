0eb9485d6f08df19bd44de3fc06e0c96619ef9d7
JsaldiasA <javier.saldias.araya@gmail.com>
2025-12-29 21:06:43 +0900

    tk2

--- TicketInicio.php
+++ TicketInicio.php
@@ -4,13 +4,19 @@
 
 require_once 'views/head.php';	
 require_once 'views/navbar.php';
+$self=$_SERVER['PHP_SELF'];
+$thispath=dirname($_SERVER['PHP_SELF']);
+$sitebasepath=$_SERVER['DOCUMENT_ROOT'];
+require_once $sitebasepath."/Model/model.php";
+
+$Model = new model();
 
 echo
     '<style>body{background-color: #191919; color: #FFFFFF}</style>
         <style>.container{background-color: #292929;color: #FFFFFF; padding: 20px; border-radius: 8px;}</style>';
 
-echo
-    '<div class="container">
+?>
+    <div class="container">
         <div class="row">
             <div class="col">
                 <br><h1><b>Tickets</b></h1><br>
@@ -22,6 +28,29 @@ echo
                 <br><br><p class="text-end"><b>Ingresar nuevo ticket.  <b><a href="../TicketForm/TicketForm.php" class="btn btn-success" role="button"> + </a>
             </div>
         </div>
-    </div>';
+        <div class="row">
+    
 
-?>
+            <table class="table">
+	        <thead>
+	        <th scope="col">Serie</th>
+            <th scope="col"></th>
+	        </thead>
+	        <tbody>
+            <?php  
+            $tickets = $Model->get_ticket();
+
+            foreach( $tickets as $ticket )
+            {   
+                echo "<tr>";
+                echo "<td>".$ticket->get_Id()."</td>"; 
+                echo "<td>". $ticket->get_Nombre()."</td>";
+                echo "<td>". $ticket->get_Ubicacion()."</td>";
+                echo "<td>". $ticket->get_FechaInicio()."</td>";
+                echo "</tr>";
+            }
+            ?>
+            </tbody></table>
+
+        </div>
+    </div>
