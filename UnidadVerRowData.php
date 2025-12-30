07899448ecc71b100370b92228af2b644ea85c04
JsaldiasA <javier.saldias.araya@gmail.com>
2025-12-30 23:15:35 +0900

    test

--- UnidadVerRowData.php
+++ UnidadVerRowData.php
@@ -1,26 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
 <?php
 
-require_once 'views/head.php';	
-require_once 'views/navbar.php';
 require_once 'views/page.php';
-$self=$_SERVER['PHP_SELF'];
-$thispath=dirname($_SERVER['PHP_SELF']);
-$sitebasepath=$_SERVER['DOCUMENT_ROOT'];
-require($sitebasepath.'/vendor/autoload.php');
-require_once $sitebasepath."/Model/model.php";
 
-$Model = new model();
+$Page = new page();
 
-$sql = "SELECT * FROM `rowdata` ORDER BY `id` DESC LIMIT 10";
+$Model = $Page->get_Model();
+
+$sql = "SELECT * FROM `rowdata` ORDER BY `id` DESC LIMIT 500";
 $result = $Model->executeSQL($sql);
 
 $HtmlPage ='<div class="container">
 	<div class="row">
 	<div class="col">';
 
-$HtmlPage = $HtmlPage. '<table id="TablaRegistros" class="table table-striped table-hover">
+$HtmlPage = $HtmlPage. '<table class="table">
 			  <thead>
 			  <th scope="col">Id</th>
 			  <th scope="col">DateTime</th>
@@ -29,33 +22,22 @@ $HtmlPage = $HtmlPage. '<table id="TablaRegistros" class="table table-striped ta
 			  </thead>
 			  <tbody>';
 
-if ($result->num_rows > 0)
-	{
+if ($result->num_rows > 0){
     	while($row = $result->fetch_assoc()) 
 			{
 				$HtmlPage = $HtmlPage. '<tr"><td>'. $row["Id"] ." </td><td>" . $row["DateTime"] ."</td><td>" . $row["RowData"] ."</td><td>" . $row["IP"] ."</td></tr>";
 			}		
-	}
-else
-	{
+}
+else{
     	$HtmlPage = $HtmlPage. "0 results";
-	}
+}
 
 $HtmlPage = $HtmlPage. '</tbody></table>';
-
 $HtmlPage = $HtmlPage. '</div>
 	</div>
 	</div>';
 
-//$tk= $_GET['tk'];
-if (isset($_COOKIE['token'])) {
-    $tk = $_COOKIE['token'];
-} else {
-    $tk = "";
-} 
-
-$Page = new page($HtmlPage,$tk);
-
+$Page->set_PageHTML($HtmlPage);
 echo $Page->get_PageHTML();
 
 
--- views/page.php
+++ views/page.php
@@ -1,32 +1,40 @@
 <?php
 
+
 $self=$_SERVER['PHP_SELF']; 
 $thispath=dirname($_SERVER['PHP_SELF']);
 $sitebasepath=$_SERVER['DOCUMENT_ROOT'];
 
-require($sitebasepath.'/vendor/autoload.php');
+
+require_once $sitebasepath.'/views/head.php';	
+require_once $sitebasepath.'/views/navbar.php';
 require_once $sitebasepath."/helpers/JWT.php";
+require_once $sitebasepath."/Model/model.php";
+
+require($sitebasepath.'/vendor/autoload.php');
 
 class page
   {
 
     public $PageHTML;
     public $token;
+    public $Model;
 	    
-    function __construct ($PageHTML, $token)
+    function __construct ()
       { 		
-        $this->PageHTML = $PageHTML;
-        $this->token = $token;
+        $this->PageHTML = '<h1> PAGINA No agregada </h1>';
+        isset($_COOKIE['token']) ? $this->token= $_COOKIE['token'] : $this->token = "";
+        $this->Model = new Model();
       }
  
     function get_PageHTML()
-      { 
-        $jwtHelper = new JWT();
-        $decoded = $jwtHelper->decode($this->token);
+    { 
+      $jwtHelper = new JWT();
+      $decoded = $jwtHelper->decode($this->token);
+      if ($decoded) {
 
-        if ($decoded) {
         $expTime = $decoded["body"]["exp"];
-   
+    
         if ($expTime< time()) {
           return
           '<div class="container">
@@ -42,17 +50,26 @@ class page
                 </div>
               <div class="col">  
             </div>
-           </div>';
+          </div>';
           }
           else
           {
               return $this->PageHTML;
           }
-        } else {
-            return  "Invalid token.";
-        }      
-      }
+      } else {
+          return  "Invalid token.";
+      }      
+    }
 
+    function set_PageHTML($HTML)
+    { 
+       $this->PageHTML = $HTML; 
+    }
+      
+    function get_Model()
+    {     
+      return $this->Model;
+    }
   }
 
 
