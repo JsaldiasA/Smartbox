88bab25a11d7044aebdc97353c6db23205135e3a
JsaldiasA <javier.saldias.araya@gmail.com>
2026-01-02 23:59:27 +0900

    asdasd

--- views/navbar.php
+++ views/navbar.php
@@ -63,28 +63,6 @@ function myFunction() {
 
 
 </style>
-<div class="topnav" id="myTopnav">
-  <div class="row">
-    <div class="col">
-      
-    </div>
-    <div class="col">
-    <a  href="../main.php" >Home</a>
-    </div>
-    <div class="col">
-    </div>
-    <a  href="../UnidadVerRowData.php" >RowData</a>
-    </div>
-    <div class="col">
-    <a  href="../TicketInicio.php" >Tickets</a>
-    </div>
-    <div class="col">  
-    <a  href="../Sirecor2.0/views/riego/index.php" >Docs<a>
-    </div>
-  </div>
-</div>
-
-
 <nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
   <img src="../images/LogoPrincipal.png"  style="margin-left: 0px;width:100px;height:auto;">
