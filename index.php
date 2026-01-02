641a9518441cf5367051f4d077acc587436b40a8
JsaldiasA <javier.saldias.araya@gmail.com>
2026-01-02 23:01:06 +0900

    fix

--- index.php
+++ index.php
@@ -42,8 +42,8 @@ function Login()
 
 	<div class="container-fluid vh-100 d-flex align-items-center justify-content-center">
 		<!-- Small Container (Width controlled by col-md-4) -->
-			<div class="card shadow-lg p-4 border-0 rounded-5 " style="width: 36 rem;" >
-					<div class="mx-5">
+			<div class="card shadow-lg p-4 border-0 rounded-5 " style="width: 36rem;" >
+					<div class="m-5">
 						<img src="../images/LogoPrincipal.png" class="card-img-top"  >
 					</div>
 					<div class="card-body m-3 ">
