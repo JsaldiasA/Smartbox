107bdc78ab22ad959965ea0f439e2dff12bac360
hsuarezeco3 <hsuarez@eco3.cl>
2026-01-05 10:16:33 -0300

    voltbat2

--- ChecklistForm/CheckListForm.php
+++ ChecklistForm/CheckListForm.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
+
 $self=$_SERVER['PHP_SELF']; 
 $thispath=dirname($_SERVER['PHP_SELF']);
 $sitebasepath=$_SERVER['DOCUMENT_ROOT'];
@@ -8,99 +9,133 @@ require_once $sitebasepath."/views/head.php";
 require_once $sitebasepath."/views/navbar.php";
 require_once $sitebasepath."/Model/model.php";
 
-?>
-<?php
-
 $UnidadTag= $_GET['tag'];
 $Model = new Model();
 $unidadDbEntity=$Model->unidadByTag($UnidadTag);
-	?>	
-	
+
+?>	
 <script>
 
-	function FunctionNuevoCheckListPost() {
-		let text = "¿Está seguro de enviar el CheckList?";
-		if (confirm(text) == true) { 
-		
-		let pattern = /(^\d+\.\d+$)|(^\d+$)/; 
-		var URL = "NuevoCheckListPost.php"; 
-		var Respuesta;
-		var token = "eco3spa";
-		var noAjustarMsg = " ,vuelva a ajustarlo. SI NO PUEDE AJUSTARLO NO UTILICE ESTA PLACA EN TERRENO, póngase en contacto con la oficina técnica.";
-		var IMEI= "<?php echo $unidadDbEntity->get_tag();?>";
-		var id_unidad= "<?php echo $unidadDbEntity->get_id();?>";
-			
-		var VoltajeReguladorBat= document.getElementById("VoltajeReguladorBat");
-		var hasError = NumericParameterHasError(VoltajeReguladorBat,14.2,12.4) ; 
-		
-		if(hasError){document.getElementById('VoltajeReguladorBat').className += ' border border-danger';return;}
-		else {document.getElementById('VoltajeReguladorBat').className='form-control';}  
-	
-		var VoltajeReguladorMCU= document.getElementById("VoltajeReguladorMCU");
-		hasError = NumericParameterHasError(VoltajeReguladorMCU,5.3,5) ; 	    
-		
-		if(hasError){document.getElementById('VoltajeReguladorMCU').className += ' border border-danger';return;}
-		else {document.getElementById('VoltajeReguladorMCU').className='form-control';}  
-		
-		var SmartBox= Number(document.getElementById("SmartBox").checked);
-		var SMSenvio= Number(document.getElementById("SMSenvio").checked);
-		var SMSrecibo= Number(document.getElementById("SMSrecibo").checked);
-		var Flujometro= Number(document.getElementById("Flujometro").checked);
-		var Solenoide= Number(document.getElementById("Solenoide").checked);
-		var SensorNivelBajo= Number(document.getElementById("SensorNivelBajo").checked);
-		var SensorNivelAlto= Number(document.getElementById("SensorNivelAlto").checked);   
-		var VoltajeMCU= document.getElementById("VoltajeMCU");
-		hasError = NumericParameterHasError(VoltajeMCU,4,3.3) ; 	    
-		
-		if(hasError){document.getElementById('VoltajeMCU').className += ' border border-danger';return;}
-		else {document.getElementById('VoltajeMCU').className='form-control';} 
-		
-		var BateriaTest= Number(document.getElementById("BateriaTest").checked);
-		var e = document.getElementById("ChecklistMotivo");
-		var id_checklistMotivo = e.options[e.selectedIndex].value;  
-		var Observaciones= document.getElementById("Observaciones").value;
-		if(Observaciones == "" ){ return alert("Observaciones no puede estar vacío, coloque alguna observación. Si no tiene coloque OK");}    
-		var f = document.getElementById("unidadtipo");
-		var id_unidadtipo = f.options[f.selectedIndex].value;   
-		var TecnicoResponsable= document.getElementById("TecnicoResponsable").value;
-		if(TecnicoResponsable == "" ){ return alert("Técnico responsable no puede estar vacío, coloque su nombre.");}
-		var URL_foto= document.getElementById("NombreDeFoto").innerHTML;	  
-	  
-		$.ajax({
-            url:URL,    //the page containing php script
-            type: "post",    //request 
-			dataType: 'text',
-			data: {
-			token: token,
-		    IMEI: IMEI,
-			id_unidad: id_unidad,
-			VoltajeReguladorBat: VoltajeReguladorBat.value,
-			VoltajeReguladorMCU: VoltajeReguladorMCU.value,
-			SmartBox: SmartBox,
-			SMSenvio: SMSenvio,
-			SMSrecibo: SMSrecibo,
-			Flujometro: Flujometro,
-			Solenoide: Solenoide,
-			SensorNivelBajo: SensorNivelBajo,
-			SensorNivelAlto: SensorNivelAlto,
-			VoltajeMCU: VoltajeMCU.value,
-			BateriaTest: BateriaTest,
-			id_checklistMotivo: id_checklistMotivo,
-			Observaciones: Observaciones,
-			id_unidadtipo: id_unidadtipo,
-			TecnicoResponsable: TecnicoResponsable,
-			URL_foto: URL_foto,	    
-        	},
-		    success: function(result){alert(result);
-			window.location.href = "https://smartbox.eco3.cl/main.php";
-			}    
-		  });
+	function FunctionNuevoCheckListPost()
+		{
+			let text = "¿Está seguro de enviar el CheckList?";
+			if (confirm(text) == true)
+				{
+					let pattern = /(^\d+\.\d+$)|(^\d+$)/;
 
-		//window.location.href = "https://smartbox.eco3.cl/";
-		} else {
-			alert("La operación se ha cancelado.");
+					var URL = "NuevoCheckListPost.php"; 
+					var Respuesta;
+					var token = "eco3spa";
+					var noAjustarMsg = " ,vuelva a ajustarlo. SI NO PUEDE AJUSTARLO NO UTILICE ESTA PLACA EN TERRENO, póngase en contacto con la oficina técnica.";
+					var IMEI= "<?php echo $unidadDbEntity->get_tag();?>";
+					var id_unidad= "<?php echo $unidadDbEntity->get_id();?>";
+					var VoltajeReguladorBat= document.getElementById("VoltajeReguladorBat");
+					var hasError = NumericParameterHasError(VoltajeReguladorBat,14.2,12.4) ; 
+			
+					if(hasError)
+						{
+							document.getElementById('VoltajeReguladorBat').className += ' border border-danger';return;
+						}
+					else
+						{
+							document.getElementById('VoltajeReguladorBat').className='form-control';
+						}  
+		
+					var VoltajeReguladorMCU= document.getElementById("VoltajeReguladorMCU");
+					hasError = NumericParameterHasError(VoltajeReguladorMCU,5.3,5) ; 	    
+			
+					if(hasError)
+						{
+							document.getElementById('VoltajeReguladorMCU').className += ' border border-danger';return;
+						}
+					else
+						{
+							document.getElementById('VoltajeReguladorMCU').className='form-control';
+						}  
+			
+					var SmartBox= Number(document.getElementById("SmartBox").checked);
+					var SMSenvio= Number(document.getElementById("SMSenvio").checked);
+					var SMSrecibo= Number(document.getElementById("SMSrecibo").checked);
+					var Flujometro= Number(document.getElementById("Flujometro").checked);
+					var Solenoide= Number(document.getElementById("Solenoide").checked);
+					var SensorNivelBajo= Number(document.getElementById("SensorNivelBajo").checked);
+					var SensorNivelAlto= Number(document.getElementById("SensorNivelAlto").checked);   
+					var VoltajeMCU= document.getElementById("VoltajeMCU");
+					hasError = NumericParameterHasError(VoltajeMCU,4,3.3);
+
+					if(hasError)
+						{
+							document.getElementById('VoltajeMCU').className += ' border border-danger';return;
+						}
+					else
+						{
+							document.getElementById('VoltajeMCU').className='form-control';
+						} 
+			
+					var VoltajeBateria= document.getElementById("VoltajeBateria");
+					hasError = NumericParameterHasError(VoltajeBateria,4.2,3.6);
+
+					var BateriaTest= Number(document.getElementById("BateriaTest").checked);
+					var e = document.getElementById("ChecklistMotivo");
+					var id_checklistMotivo = e.options[e.selectedIndex].value;  
+					var Observaciones= document.getElementById("Observaciones").value;
+					if(Observaciones == "" )
+						{
+							return alert("Observaciones no puede estar vacío, coloque alguna observación. Si no tiene coloque OK");
+						}
+
+					var f = document.getElementById("unidadtipo");
+					var id_unidadtipo = f.options[f.selectedIndex].value;   
+					var TecnicoResponsable= document.getElementById("TecnicoResponsable").value;
+					if(TecnicoResponsable == "" )
+						{
+							return alert("Técnico responsable no puede estar vacío, coloque su nombre.");
+						}
+
+					var URL_foto= document.getElementById("NombreDeFoto").innerHTML;	  
+		
+					$.ajax(
+						{
+							url:URL,    //the page containing php script
+							type: "post",    //request 
+							dataType: 'text',
+							data:
+								{
+									token: token,
+									IMEI: IMEI,
+									id_unidad: id_unidad,
+									VoltajeReguladorBat: VoltajeReguladorBat.value,
+									VoltajeReguladorMCU: VoltajeReguladorMCU.value,
+									SmartBox: SmartBox,
+									SMSenvio: SMSenvio,
+									SMSrecibo: SMSrecibo,
+									Flujometro: Flujometro,
+									Solenoide: Solenoide,
+									SensorNivelBajo: SensorNivelBajo,
+									SensorNivelAlto: SensorNivelAlto,
+									VoltajeMCU: VoltajeMCU.value,
+									VoltajeBateria: VoltajeBateria.value,
+									BateriaTest: BateriaTest,
+									id_checklistMotivo: id_checklistMotivo,
+									Observaciones: Observaciones,
+									id_unidadtipo: id_unidadtipo,
+									TecnicoResponsable: TecnicoResponsable,
+									URL_foto: URL_foto,	    
+								},
+							success: function(result)
+								{
+									alert(result);
+									window.location.href = "https://smartbox.eco3.cl/main.php";
+								}    
+						});
+
+						//window.location.href = "https://smartbox.eco3.cl/";
+				}
+			else
+				{
+					alert("La operación se ha cancelado.");
+				}
 		}
-	}
 
 
 
@@ -173,6 +208,7 @@ $unidadDbEntity=$Model->unidadByTag($UnidadTag);
 		<tr><td><b>Flujómetro:</b></td><td><input type="checkbox" class="form-check-input" id="Flujometro"></td><td></td></tr>
 		<tr><td><b>Sensor nivel bajo:</b></td><td><input type="checkbox" class="form-check-input" id="SensorNivelBajo"></td><td></td></tr>
 		<tr><td><b>Sensor nivel alto:</b></td><td><input type="checkbox" class="form-check-input" id="SensorNivelAlto" ></td><td></td></tr>
+		<tr><td><b>Voltaje de la batería:</b></td><td><input type="checkbox" class="form-check-input" id="VoltajeBateria" ></td><td></td></tr>
 		<tr><td><b>Medidor de batería:</b></td><td><input type="checkbox" class="form-check-input" id="BateriaTest" ></td><td></td></tr>
 		<tr><td><b>Observaciones:</b></td><td><input type="text" class="form-control" id="Observaciones" placeholder="Si no tiene comentarios, coloque OK."></td><td></td></tr>
 		<tr><td><b>Técnico responsable:</b></td><td><input type="text" class="form-control" id="TecnicoResponsable" placeholder="Nombre"></td><td></td></tr>
