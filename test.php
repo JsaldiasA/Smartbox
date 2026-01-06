0614dc91406755d0b18709f40db4878266800de1
JsaldiasA <javier.saldias.arya@gmail.com>
2026-01-06 21:08:38 +0900

    gtest

---
+++ test.php
@@ -0,0 +1,10 @@
+ <?php
+$self=$_SERVER['PHP_SELF'];
+$thispath=dirname($_SERVER['PHP_SELF']);
+$sitebasepath=$_SERVER['DOCUMENT_ROOT'];
+require_once $sitebasepath."/Model/model.php";
+
+$model = new Model();
+echo var_dump($model->get_ticket());
+
+?>
