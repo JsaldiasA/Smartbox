4b0a8204e85d158e4ff1f5d3a17259772f7375be
JsaldiasA <javier.saldias.arya@gmail.com>
2026-01-07 22:13:25 +0900

    tk update

---
+++ TicketForm/TicketFormUpdate.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang = "en">
+<?php
+
+$self = $_SERVER['PHP_SELF']; 
+$thispath = dirname($_SERVER['PHP_SELF']);
+$sitebasepath = $_SERVER['DOCUMENT_ROOT'];
+
+require_once $sitebasepath."/views/head.php";
+require_once $sitebasepath."/views/navbar.php";
+require_once $sitebasepath."/Model/model.php";
+
+echo '<style>body{background-color: #191919; color: #FFFFFF}</style>
+		<style>.container{background-color: #292929;color: #FFFFFF;padding: 20px;border-radius: 8px;}</style>';
+
+$Model = new Model();
+
+$id_ticket = $_GET['id_ticket'];
+
+$ticket = $Model->ticketById($id_ticket);		
+
+?>
+
+<script>
+
+	function FunctionNuevoTicketPost()
+		{
+			let text = "¿Está seguro de enviar el ticket?";
+			if (confirm(text) == true)
+				{
+					let pattern = /(^\d+\.\d+$)|(^\d+$)/;
+					var URL = "/ApiController/ticket/TicketUpdate.php";
+					var Respuesta;
+
+					var Nombre = document.getElementById("Nombre").value;
+					if (Nombre == "")
+						{
+							return alert ("Debe especificar un dispositivo y/o plataforma.");
+						}
+
+	  				var Ubicacion = document.getElementById("Ubicacion").value;
+					if (Ubicacion == "")
+						{
+							return alert ("Debe especificar un dispositivo y/o plataforma.");
+						}
+
+					var Descripcion = document.getElementById("Descripcion").value;
+					if (Descripcion == "" )
+						{
+							return alert ("Debe explicar de que se trata el problema.");
+						}
+
+					var Usuario = document.getElementById("Usuario").value;
+					if (Usuario == "" )
+						{
+							return alert ("Debe escribir su nombre.");
+						}
+
+					$.ajax(
+						{
+            				url:URL,
+            				type:"post",
+							dataType:'text',
+							data:
+								{
+									Nombre: Nombre,
+									Ubicacion: Ubicacion,
+									Descripcion: Descripcion,
+									Usuario: Usuario,
+        						},
+							success: function(result)
+								{
+									alert (result)
+								}
+						});
+
+					//window.location.href = "https://smartbox.eco3.cl/";
+  				}
+			else
+				{
+    				alert ("La operación se ha cancelado.");
+  				}
+		}
+
+</script>
+
+<?php
+
+	echo
+		'<div class="container">
+			<div class="row">
+				<div class="col">
+					<b><h1>Nuevo formulario: <br></h1></b>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<b>Asunto: </b>
+				</div>
+				<div class="col">
+					<input type="text" class="form-control" id="Nombre" value="'.$ticket->Nombre.'"><br>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col">
+					<b>Ubicación: </b>
+				</div>
+				<div class="col">
+					<input type="text" class="form-control" id="Ubicacion" value="'.$ticket->Ubicacion.'"><br>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col">
+					<b>Descripción: </b>
+				</div>
+				<div class="col">
+					<input type="text" class="form-control" style="height: 200px; width: 100%" id="Descripcion" value="'.$ticket->Descripcion.'"><br>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col">
+					<b>Usuario: </b>
+				</div>
+				<div class="col">
+					<input type="text" class="form-control" id="Usuario" value="'.$ticket->Usuario.'><br>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col">
+					<button type="button" class="btn btn-success" onclick="FunctionUpdateTicketPost()">Enviar ticket</button>
+				</div>
+			</div>
+		</div>';
+
+?>
