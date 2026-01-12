cafd874a0e5b5d1c22712d395135024c91e5a8c4
JsaldiasA <javier.saldias.arya@gmail.com>
2026-01-13 00:07:25 +0900

    fix

--- ApiController/GetUnidad.php
+++ ApiController/GetUnidad.php
@@ -100,7 +100,7 @@ foreach ($UnidadesFiltradasPorTipo as $unidad)
     echo "<td>".$unidad->DiffBetweenNow_and_UltimaActualizacion()."</td>";
     echo "<td>".$unidad->get_Estado()  ."</td>";
     echo "<td>".$BatNivel->get_HtmlTableField()."</td>";
-	echo !$isEstanque ? ($ultimoRegistro == null ? "<td>0</td>":"<td>".$ultimoRegistro->get_CAUDAL()."</td>"): null;
+	echo !$isEstanque ? ($ultimoRegistro == null ? "<td>0</td>":"<td>".$ultimoRegistro->CAUDAL."</td>"): null;
     echo !$isEstanque ? ($IsMilesight ?  "<td>".$unidad->get_VolumenForMilesight()."</td>":"<td>".$unidad->get_Volumen()."</td>" ): null;
     echo "<td> <a href='unidadver.php?tag=".$unidad->get_Tag()."'>Ver</a></td></tr>";
 }
