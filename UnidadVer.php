0596dc271a86852b16bb8f41324e87a070946f7d
JsaldiasA <javier.saldias.arya@gmail.com>
2026-01-15 00:21:19 +0900

    test

--- UnidadVer.php
+++ UnidadVer.php
@@ -419,7 +419,7 @@ $HtmlPage=$HtmlPage.'
 
 
 		foreach ($SMS as $s) {
-		$HtmlPage=$HtmlPage. '<tr>  <td>'. $s->Id. "</td><td>" . $s->SMS ."</td> <td>" . $s->Recibido."</td> <td>" . $s->CreateTime ."</td><td><div onclick=\"FunctionDeleteSMS(\"".$s->Id."\")\">".$deleteIcon."</div></td></tr>";
+		$HtmlPage=$HtmlPage. '<tr>  <td>'. $s->Id. "</td><td>" . $s->SMS ."</td> <td>" . $s->Recibido."</td> <td>" . $s->CreateTime ."</td><td><button type=\"button\" class=\"btn btn-outline-danger\" onclick=\"FunctionDeleteSMS(\"".$s->Id."\")\" >".$deleteIcon."</button></td></tr>";
 		
 		if ($counter >= 5) {
         break; // Terminate the loop after the limit is reached
