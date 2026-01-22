3d5083661c3345cb7de3c6b1dba75fab68a9dfe9
JsaldiasA <javier.saldias.arya@gmail.com>
2026-01-22 23:23:42 +0900

    ahora si

--- UnidadVerChecklist.php
+++ UnidadVerChecklist.php
@@ -11,54 +11,41 @@ require_once 'views/navbar.php';
 <?php
 
 $CheckList_Id= $_GET['CheckList_Id'];
-
-require_once 'DbEntities/unidadDbEntity.php';
-require_once 'DbEntities/checklistDbEntity.php';
-require_once 'config/DbSirecorConfig.php';
 require_once 'Model/model.php';
 
-$Model = new Model();
+$Model = new Model() ;
 // Create connection
-
-
-$HasChecklist = true;	
 
 $checklistDbEntity= $Model->checklistsWHERE('Id',$CheckList_Id)[0];
 
-if($HasChecklist == true)
-	{   
 		//IMPRIMIMOS  cabecera de informacion con los datos de la unidad desde la tabla unidad
 		echo '<div class="container">';// container cabecera de informacion	
 		echo '<br><h1><b>CheckList</b></h1>';
 		// IMPRIMIMOS TABLA CON LOS DATOS DE LA UNIDAD.
 		echo '<table class="table">
 		<tbody>';// Header tabla
-		echo "<tr><td><b>ID:</b></td><td>". $checklistDbEntity->get_Id(). "</td></tr>";
-		echo "<tr><td><b>Técnico responsable:</b></td><td>". $checklistDbEntity->get_TecnicoResponsable(). "</td></tr>";
-		$checklistMotivo = $Model->CheckListMotivoById($checklistDbEntity->get_id_checklistMotivo());
-		echo "<tr><td><b>Motivo del checklist:</b></td><td>". $checklistMotivo->get_Nombre(). "</td></tr>";
-		echo "<tr><td><b>Tipo de unidad: </b></td><td>". $checklistDbEntity->get_id_unidadtipo(). "</td></tr>";
-		echo "<tr><td><b>Voltaje regulador de batería:</b></td><td>". $checklistDbEntity->get_VoltajeReguladorBat(). "</td></tr>";
-		echo "<tr><td><b>Voltaje regulador de MCU:</b></td><td>". $checklistDbEntity->get_VoltajeReguladorMCU(). "</td></tr>";
-		echo "<tr><td><b>Voltaje MCU:</b></td><td>". $checklistDbEntity->get_VoltajeMCU(). "</td></tr>";
-		echo "<tr><td><b>SmartBox:</b></td><td>". $checklistDbEntity->get_SmartBox(). "</td></tr>";
-		echo "<tr><td><b>Envío SMS:</b></td><td>". $checklistDbEntity->get_SMSenvio(). "</td></tr>";
-		echo "<tr><td><b>Recepción SMS:</b></td><td>". $checklistDbEntity->get_SMSrecibo(). "</td></tr>";
-		echo "<tr><td><b>Solenoide:</b></td><td>". $checklistDbEntity->get_Solenoide(). "</td></tr>";
-		echo "<tr><td><b>Flujómetro:</b></td><td>". $checklistDbEntity->get_Flujometro(). "</td></tr>";
-		echo "<tr><td><b>Sensor nivel bajo:</b></td><td>". $checklistDbEntity->get_SensorNivelBajo(). "</td></tr>";
-		echo "<tr><td><b>Sensor nivel alto:</b></td><td>". $checklistDbEntity->get_SensorNivelAlto(). "</td></tr>";
-		echo "<tr><td><b>Voltaje de la batería:</b></td><td>". $checklistDbEntity->get_VoltajeBateria(). "</td></tr>";
-		echo "<tr><td><b>Medidor de batería:</b></td><td>". $checklistDbEntity->get_BateriaTest(). "</td></tr>";
-		echo "<tr><td><b>Observaciones:</b></td><td>". $checklistDbEntity->get_Observaciones(). "</td></tr>";
-		echo "<tr><td><img src='". $checklistDbEntity->get_URL_foto() ."' class='img-fluid' ></td></tr>";
+		echo "<tr><td><b>ID:</b></td><td>". $checklistDbEntity-> Id . "</td></tr>";
+		echo "<tr><td><b>Técnico responsable:</b></td><td>". $checklistDbEntity-> TecnicoResponsable . "</td></tr>";
+		$checklistMotivo = $Model->CheckListMotivoById($checklistDbEntity-> id_checklistMotivo );
+		echo "<tr><td><b>Motivo del checklist:</b></td><td>". $checklistMotivo-> Nombre . "</td></tr>";
+		echo "<tr><td><b>Tipo de unidad: </b></td><td>". $checklistDbEntity-> id_unidadtipo . "</td></tr>";
+		echo "<tr><td><b>Voltaje regulador de batería:</b></td><td>". $checklistDbEntity-> VoltajeReguladorBat . "</td></tr>";
+		echo "<tr><td><b>Voltaje regulador de MCU:</b></td><td>". $checklistDbEntity-> VoltajeReguladorMCU . "</td></tr>";
+		echo "<tr><td><b>Voltaje MCU:</b></td><td>". $checklistDbEntity-> VoltajeMCU . "</td></tr>";
+		echo "<tr><td><b>SmartBox:</b></td><td>". $checklistDbEntity-> SmartBox . "</td></tr>";
+		echo "<tr><td><b>Envío SMS:</b></td><td>". $checklistDbEntity-> SMSenvio . "</td></tr>";
+		echo "<tr><td><b>Recepción SMS:</b></td><td>". $checklistDbEntity-> SMSrecibo . "</td></tr>";
+		echo "<tr><td><b>Solenoide:</b></td><td>". $checklistDbEntity-> Solenoide . "</td></tr>";
+		echo "<tr><td><b>Flujómetro:</b></td><td>". $checklistDbEntity-> Flujometro . "</td></tr>";
+		echo "<tr><td><b>Sensor nivel bajo:</b></td><td>". $checklistDbEntity-> SensorNivelBajo . "</td></tr>";
+		echo "<tr><td><b>Sensor nivel alto:</b></td><td>". $checklistDbEntity-> SensorNivelAlto . "</td></tr>";
+		echo "<tr><td><b>Voltaje de la batería:</b></td><td>". $checklistDbEntity-> VoltajeBateria . "</td></tr>";
+		echo "<tr><td><b>Medidor de batería:</b></td><td>". $checklistDbEntity-> BateriaTest . "</td></tr>";
+		echo "<tr><td><b>Observaciones:</b></td><td>". $checklistDbEntity-> Observaciones . "</td></tr>";
+		echo "<tr><td><img src='". $checklistDbEntity-> URL_foto  ."' class='img-fluid' ></td></tr>";
 		echo '</tbody></table>';
 		echo '</div>';
-	}	
-else
-	{
-		echo '<h1>Checklist no encontrado.</h1>';
-	}
+
 ?>
 </body>
 </html>
