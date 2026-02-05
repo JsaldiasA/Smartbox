d765e54c076524835a999c5ed63ffd84f3264abb
JsaldiasA <javier.saldias.arya@gmail.com>
2026-02-05 22:26:08 +0900

    no duplicate message

--- ApiController/SMSToUnidades/SMSToUnidadesCreate.php
+++ ApiController/SMSToUnidades/SMSToUnidadesCreate.php
@@ -7,25 +7,60 @@ $sitebasepath=$_SERVER['DOCUMENT_ROOT'];
 require_once $sitebasepath."/Model/model.php";
 
 $model = new Model();
-$SMS = new smstounidadesDbEntity();
+$NewSMS = new smstounidadesDbEntity();
 
-$allKeys = array_keys((array)$SMS);
+$allKeys = array_keys((array)$NewSMS);
 
 foreach ($allKeys as $key ) 
 {
-	$SMS->$key = (($_POST[$key] == null)? 'NULL': $_POST[$key]);
+	$NewSMS->$key = (($_POST[$key] == null)? 'NULL': $_POST[$key]);
 }
 
 // SET Default Recibido = 0 autoincrement
-$SMS->Recibido = '0';
+$NewSMS->Recibido = '0';
 // SET Default Id = 0 autoincrement
-$SMS->Id = '0';
+$NewSMS->Id = '0';
 // SET creationtime
 date_default_timezone_set('America/Santiago');
-$SMS->CreateTime = date("Y-m-d H:i:s");
+$NewSMS->CreateTime = date("Y-m-d H:i:s");
 
-$model->MYSQLInsertInto('smstounidades',$SMS);
-echo 'alert(SMS creado Correctamente)';
-echo var_dump($SMS);
+// Check if the mesage is duplicated
+
+$isDuplicated = false;
+
+$smsForThisUnit = $model->MYSQLSelectWHERE('smstounidades','Id_unidad',$NewSMS->Id_unidad);
+$smsNoReceived = [];
+
+foreach($smsForThisUnit as $sms)
+{
+	if($sms->Recibido == '0' )
+	{
+		$smsNoReceived [] = $sms;
+	}
+}
+
+if($smsNoReceived != null)
+{
+	foreach($smsNoReceived as $sms)
+	{
+		if($sms->SMS == $NewSMS->SMS)
+		{
+			$isDuplicated = true;
+		}
+	}
+
+}
+
+if($isDuplicated == false && $NewSMS->SMS != null)
+{
+	$model->MYSQLInsertInto('smstounidades',$NewSMS);
+	echo 'SMS creado Correctamente';
+	echo var_dump($NewSMS);
+}
+else{
+	echo 'Error, mensaje duplicado o el mensaje es NULL';
+}
+
+
 
 ?>
