e13ec8386098e0c6b0e47bdce441dddddd03ba95
JsaldiasA <javier.saldias.arya@gmail.com>
2026-02-06 22:50:11 +0900

    fix11

--- UnidadVer.php
+++ UnidadVer.php
@@ -92,7 +92,7 @@ $HtmlPage=$HtmlPage.'
 				}
 		}	
 		
-		$UltimoRegistroIniciacion = $Model->MYSQLSelectWHERE('eventos','UNIDAD', $unidadDbEntity->get_Tag() )[0] ;
+		//$UltimoRegistroIniciacion = $Model->MYSQLSelectWHERE('eventos','UNIDAD', $unidadDbEntity->get_Tag() )[0] ;
 
 			$HtmlPage=$HtmlPage. '<table class="table" >
 		  <tbody>';
@@ -100,7 +100,7 @@ $HtmlPage=$HtmlPage.'
 	$HtmlPage=$HtmlPage. '<tr><td><b>Volumen:</b></td><td>'.$ultimoRegistro->VOLUMEN.'</td></tr>';
 	$HtmlPage=$HtmlPage. '<tr><td><b>Caudal:</b></td><td>'.$ultimoRegistro->CAUDAL.'</td></tr>';
 	$HtmlPage=$HtmlPage. '<tr><td><b>Última Registro:</b></td><td>'.$ultimoRegistro->DATETIME.'</td></tr>';
-	$HtmlPage=$HtmlPage. '<tr><td><b>Modo</b></td><td>'. $UltimoRegistroIniciacion->INTERNET .'</td></tr>';
+	//$HtmlPage=$HtmlPage. '<tr><td><b>Modo</b></td><td>'. $UltimoRegistroIniciacion->INTERNET .'</td></tr>';
 	$HtmlPage=$HtmlPage. '</tbody></table>';
 
 	$HtmlPage=$HtmlPage. '<table class="table" >
