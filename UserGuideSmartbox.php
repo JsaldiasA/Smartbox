2922accc275ed65e4de349b9547aed7cefc8848d
JsaldiasA <javier.saldias.arya@gmail.com>
2026-02-12 21:22:54 +0900

    docs

--- UserGuideSmartbox.php
+++ UserGuideSmartbox.php
@@ -39,6 +39,34 @@ $Model = $Page->get_Model();
         <p class="h2 m-3" id="T1" >T1 Modo de la unidad </p>
         <p class="m-3" style="white-space: pre-line;">
 
+        Las unidades sirecor desde la version 4.6s es posible controlarlas desde internet. Para ver la version actual de la unidad vea  <a href="#T1"> T4 Registros iniciacion <a/>. 
+        La Unidad funciona de manera asincrona,cuando la caja envia un mensaje recibe como respuesta el mensaje de control. 
+        
+        Modo riego off 
+
+        La Frecuencia en la cual la caja envia mensajes es de 5 minutos, si queremos abrir la caja tenemos que esperar al lo mas 5 minitos para que la caja envie el mensaje. Cuando la caja esta abierta la frecuencia automaticamente se cambia a 1 minuto.
+        
+        Frecuencia de mensajes: 5 minutos cuando esta off, 1 minuto cuando esta on
+        Recomendable: durante jornada normal.
+
+        Modo riego On 
+
+        Para tener una respuesta mas instantanea para regar, podemos cambiar la caja a modo riego, la frecuencia de mensajes sera siempre 1 min.
+        
+        Frecuencia de mensajes: 1 minuto cuando esta off, 1 minuto cuando esta on
+        Recomendable: durante dia de riego.
+
+        Modo standby 
+
+        Debido a que las unidades tienen un maximo de 30 mb de trafico y una frecuencia de mensajes alta consume mas energia, es convieniente que se baje a 1 hora. 
+
+        Frecuencia de mensajes: 60 minutos cuando esta off, 1 minuto cuando esta on
+        Recomendable: durante fin de semana, noche, horarios no habiles y festivos.
+
+        IMPORTANTE, asegurase de cambiar el modo antes de empezar la jormanda, de lo contrario habra que esperar todo un ciclo para poder abrir un punto. Desactive el modo riego para ahorar energia y bateria.
+        
+
+
         </p>
 
         </div>
@@ -69,7 +97,7 @@ $Model = $Page->get_Model();
     <div class="row" > 
         <div class="col m-3" >
         
-        <p class="h2 m-3" id="T1" >T4 Registros iniciacion </p>
+        <p class="h2 m-3" id="T4" >T4 Registros iniciacion </p>
         <p class="m-3" style="white-space: pre-line;">
 
         </p>
@@ -80,7 +108,7 @@ $Model = $Page->get_Model();
     <div class="row" > 
         <div class="col m-3" >
         
-        <p class="h2 m-3" id="T1" >T5 Tipos Unidades </p>
+        <p class="h2 m-3" id="T5" >T5 Tipos Unidades </p>
         <p class="m-3" style="white-space: pre-line;">
 
         </p>
@@ -91,7 +119,7 @@ $Model = $Page->get_Model();
     <div class="row" > 
         <div class="col m-3" >
         
-        <p class="h2 m-3" id="T1" >T6 Alertas </p>
+        <p class="h2 m-3" id="T6" >T6 Alertas </p>
         <p class="m-3" style="white-space: pre-line;">
 
         </p>
