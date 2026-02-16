3c67c110afef20e3ff7541a25b0d225c68a2c016
JsaldiasA <javier.saldias.arya@gmail.com>
2026-02-16 22:48:39 +0900

    fix

--- ApiController/RegistrosDiarios/RegistrosDiariosGet.php
+++ ApiController/RegistrosDiarios/RegistrosDiariosGet.php
@@ -6,7 +6,7 @@ $thispath=dirname($_SERVER['PHP_SELF']);
 $sitebasepath=$_SERVER['DOCUMENT_ROOT'];
 require_once $sitebasepath."/Model/model.php";
 
-$model = new Model();
+$Model = new Model();
 
 $Id_unidad= $_POST['Id_unidad'];
 $return='';
