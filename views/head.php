0927e1aa2de0003fcf26116430db99ccbd6c47d4
JsaldiasA <javier.saldias.arya@gmail.com>
2026-02-18 01:09:12 +0900

    bootstrp icons

--- views/head.php
+++ views/head.php
@@ -11,6 +11,7 @@ echo'
 
 	<!-- stylesheets -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 	<link rel="stylesheet" href="https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.css">
 
 	<!-- scripts -->
