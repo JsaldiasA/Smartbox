602625568ba7ffdb4617c02538b14393b71f04c3
JsaldiasA <javier.saldias.arya@gmail.com>
2026-02-19 01:10:05 +0900

    fix2

--- ApiController/GetUnidad.php
+++ ApiController/GetUnidad.php
@@ -118,7 +118,7 @@ foreach ($UnidadesFiltradasPorTipo as $unidad)
 
 	// Print row.
     echo "<tr>";
-    echo "<td> <a href='unidadver.php?tag=".$unidad->get_Tag()."'> ...".$unidad->get_Ubicacion() ?? (substr($unidad->get_Tag() ?? "NULL", -4))."</a> </td>";
+    echo "<td> <a href='unidadver.php?tag=".$unidad->get_Tag()."'> ".$unidad->get_Ubicacion() ?? ("...". substr($unidad->get_Tag() ?? "NULL", -5))."</a> </td>";
     echo  !$IsMilesight ? ("<td>".$unidad->DiffBetweenNow_and_UltimaActualizacion()."</td>") : ('');
     echo "<td>".$unidad->get_Estado()  ."</td>";
     echo "<td>".$BatNivel->get_HtmlTableField()."</td>";
