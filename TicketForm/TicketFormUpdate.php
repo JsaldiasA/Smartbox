cf11d87c2729d861d76b535269541971c246f16f
JsaldiasA <javier.saldias.arya@gmail.com>
2026-02-20 00:57:13 +0900

    ticket fix

--- TicketForm/TicketFormUpdate.php
+++ TicketForm/TicketFormUpdate.php
@@ -122,13 +122,14 @@ $ticket = $Model->ticketById($id_ticket);
 				<div class="col">
 					<b>cuartel: </b>
 				</div>
+				<div class="col">
 			<select name="cuartel" class="form-select" id="cuartel" required>.';	
 
 					$Unidades = $Model->get_unidades();
-					foreach($Unidades as $uni){ $HtmlPage=$HtmlPage.'<option value="'.$uni->Id.'">'.$uni->Ubicacion.'</option>'; }
+					foreach($Unidades as $uni){ echo '<option value="'.$uni->Id.'">'.$uni->Ubicacion.'</option>'; }
 			
-			$HtmlPage=$HtmlPage.'</select>   </td><td></td></tr>
-			
+				echo '</select>  
+				</div>
 			</div>
 
 
