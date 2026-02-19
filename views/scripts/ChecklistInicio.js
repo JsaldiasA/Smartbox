edcbcb9676fff7e02ef466b2cca63f40c3fc5605
JsaldiasA <javier.saldias.arya@gmail.com>
2026-02-20 00:33:01 +0900

    fix4

--- views/scripts/ChecklistInicio.js
+++ views/scripts/ChecklistInicio.js
@@ -57,7 +57,7 @@ async function GetChecklistTable( ZonaName, returnJson )
 			}
 			else
 			{
-				tableHTML += '<tr class="bg-danger text-white>';
+				tableHTML += '<tr class="bg-danger text-white">';
 				tableHTML +=`<td></td>`;
 				tableHTML += `<td>${row["UnidadName"]}</td>`;
 				tableHTML += `<td>Sin checklist</td>`;
