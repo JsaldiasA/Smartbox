42b3ce05e56fa5a6622d8e70452fc91360967f24
JsaldiasA <javier.saldias.arya@gmail.com>
2026-02-21 00:46:49 +0900

    fix model

--- ApiController/Ticket/TicketCreate.php
+++ ApiController/Ticket/TicketCreate.php
@@ -6,7 +6,7 @@ $thispath=dirname($_SERVER['PHP_SELF']);
 $sitebasepath=$_SERVER['DOCUMENT_ROOT'];
 require_once $sitebasepath."/Model/model.php";
 
-$model = new Model();
+$Model = new Model();
 $newTicket = new ticketDbEntity();
 
 $allKeys = array_keys((array)$NewTicket);
