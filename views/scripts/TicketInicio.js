3f30e497da194a58074373f6628b006761c7d41a
JsaldiasA <javier.saldias.arya@gmail.com>
2026-03-02 23:59:29 +0900

    Fixtk

--- views/scripts/TicketInicio.js
+++ views/scripts/TicketInicio.js
@@ -9,6 +9,18 @@ async function GetMain(  )
 		let [Tickets, Zonas, Cuarteles, Unidades] = await Promise.all([GetTicket(), GetZonas(),GetCuarteles(),GetUnidades()]);
 
 		let tableHTML = '';
+
+		tableHTML += '	<div class="row pb-3">';
+		tableHTML += ' <div class="col p-3 card shadow p-3 card shadow">';
+		tableHTML += `    <h2><b>Open Ticket</b></h2> `;
+		tableHTML += '   <div class="overflow-auto">';
+		tableHTML += '<table class="table text-nowrap"><thead><tr>';
+		tableHTML += `<th scope="col"><i class="bi bi-pin-map"></i></th>`;
+		tableHTML += `<th scope="col">Nombre</th>`;
+		tableHTML += `<th scope="col">Fecha Inicio</th>`;
+		tableHTML += `<th scope="col">Status</th>`;
+		tableHTML += `</thead>`;
+
 		Tickets.forEach(rowTK => {
 
 			// GETTING cuertel name
@@ -24,23 +36,11 @@ async function GetMain(  )
 
 			});
 
-
-			tableHTML += '	<div class="row pb-3">';
-			tableHTML += ' <div class="col p-3 card shadow p-3 card shadow">';
-			tableHTML += `    <h2><b>Open Ticket</b></h2> `;
-			tableHTML += '   <div class="overflow-auto">';
-			tableHTML += '<table class="table text-nowrap"><thead><tr>';
-			tableHTML += `<th scope="col"><i class="bi bi-pin-map"></i></th>`;
-			tableHTML += `<th scope="col">Nombre</th>`;
-			tableHTML += `<th scope="col">Fecha Inicio</th>`;
-			tableHTML += `<th scope="col">Status</th>`;
-			tableHTML += `</thead>`;
-
 			tableHTML +='<tr>';
 			tableHTML += `<td> ${CuartelName}</td>`;
 			tableHTML += `<td>${rowTK["Nombre"]}</td>`;
 			tableHTML += `<td>${rowTK["FechaInicio"]}</td>`;
-			tableHTML += `<td>${rowTK["Id_TicketStauts"]}%</td>`;
+			tableHTML += `<td>${rowTK["Id_TicketStatus"]}</td>`;
 			tableHTML +='<tr>';
 
 			tableHTML += '</tbody></table>';
