5d5d48dd349fdc3860a71df9bd164e5082e9ea0e
JsaldiasA <javier.saldias.arya@gmail.com>
2026-03-02 23:57:39 +0900

    fixctf

--- TicketInicio.php
+++ TicketInicio.php
@@ -17,83 +17,10 @@ $Model = $Page->get_Model();
     <script src="/views/scripts/TicketInicio.js"></script>
 
     <div class="container">
-        <div class="row pb-3">
-            <div class="col p-3 card shadow p-3 card shadow">
-                <h2><b>Estado General</b></h2> 
-                <div class="overflow-auto">
-                    <div id="TableEstadoGeneral"></div>
-                </div>
-            </div>        
-        </div>
-
-                    <div id="main"></div>
-  
-  
+        <div id="main"><div class="spinner-border text-success" role="status"><span class="visually-hidden">Loading...</span></div></div>
     </div>';
 
 
-    $HtmlPage='
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <br><h1><b>Tickets</b></h1><br>
-            </div>
-            <div class="col">
-                <tr><td><b></b></td></tr>
-            </div>
-            <div class="col">
-                <br><br><p class="text-end"><b>Ingresar nuevo ticket.  <b><a href="../TicketForm/TicketForm.php" class="btn btn-success" role="button"> + </a>
-            </div>
-        </div>
-        <div class="row">
-    
-            <table class="table">
-	        <thead>
-	        <th scope="col">Id</th>
-            <th scope="col">Cuartel</th>
-            <th scope="col">Nombre</th>
-            <th scope="col">FechaInicio</th>
-            <th scope="col"></th>
-	        </thead>
-	        <tbody>';
-
-            $tickets = $Model->get_ticket();
-            $cuarteles = $Model->MYSQLSelect('cuarteles');
-
-            $cuartelAsociado;
-
-            foreach( $tickets as $ticket )
-            {   
-
-                foreach( $cuarteles as $cuartel)
-                {
-                    if( $cuartel->Id_unidad != null )
-                    {
-                        if( $cuartel->Id_unidad == $ticket->Id_unidad)
-                        {
-                            $cuartelAsociado =  $cuartel ;
-                        } 
-                    } 
-                }    
-
-                $ticketstatus = $Model->ticketStatusById( $ticket->get_Id_TicketStatus() );
-
-                $HtmlPage = $HtmlPage. "<tr>";
-                $HtmlPage = $HtmlPage. "<td>". $ticket->get_Id() ."</td>"; 
-                $HtmlPage = $HtmlPage. "<td>". ( $cuartelAsociado == null ? 'sin cuartel' : $cuartelAsociado->Name )."</td>"; 
-                $HtmlPage = $HtmlPage. "<td>". $ticket->get_Nombre() ."</td>";
-                $HtmlPage = $HtmlPage. "<td>". $ticket->get_FechaInicio() ."</td>";
-                $HtmlPage = $HtmlPage. "<td>". $ticketstatus->get_Descripcion() ."</td>";
-                $HtmlPage = $HtmlPage. "<td> <a href='Ticketver.php?id_ticket=".$ticket->get_Id()."'>Ver</a></td></tr>";
-                $HtmlPage = $HtmlPage. "</tr>";
-
-                $cuartelAsociado = null;
-
-            }
-
-            $HtmlPage = $HtmlPage.'</tbody></table>
-                                    </div>
-                                    </div>';
 
             $Page->set_PageHTML($HtmlPage);
             echo $Page->get_PageHTML();
