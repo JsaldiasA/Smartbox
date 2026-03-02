7cf5b414f5d306625c0db54ac13450619a4069e5
JsaldiasA <javier.saldias.arya@gmail.com>
2026-03-02 22:10:29 +0900

    fix

--- views/scripts/ChecklistInicio.js
+++ views/scripts/ChecklistInicio.js
@@ -156,7 +156,7 @@ async function GetChecklistTables( )
 						{
 							tableHTML += '<tr class="bg-danger text-white">';
 							tableHTML += `<td>${rowCT["Name"]}</td>`;
-							tableHTML += `<td>Sin checklist</td>`;
+							tableHTML += `<td>${rowCT["Name"] == null ? 'Sin Unidad' : 'Sin Checklist'}<</td>`;
 							tableHTML += `<td></td>`;
 							tableHTML += `<td></td>`;
 							tableHTML += `<td></td>`;
