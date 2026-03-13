ad4d98f8ce61ef251348a6b1e2b8e40237777495
JsaldiasA <javier.saldias.arya@gmail.com>
2026-03-13 20:20:41 +0900

    fix

--- SprintInicio.php
+++ SprintInicio.php
@@ -12,7 +12,7 @@ $Model = $Page->get_Model();
     <div class="container">
         <div class="row pb-3">
             <div class="col p-3 card shadow p-3 card shadow">
-              <iframe src="https://docs.google.com/spreadsheets/d/1u6N9Kf1icpXGGutgmdJMsdKN_3U7vZC-/edit?usp=sharing&ouid=108650448787646658808&rtpof=true&sd=true" width="100%" height="500">
+              <iframe src="https://docs.google.com/spreadsheets/d/1u6N9Kf1icpXGGutgmdJMsdKN_3U7vZC-/edit?usp=sharing&ouid=108650448787646658808&rtpof=true&sd=true" width="100%" height="90%">
                 </iframe>
             </div>        
         </div>
