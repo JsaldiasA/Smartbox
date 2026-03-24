8f7a9d0d9405986a51e687ced7c4c1292d6bc83f
JsaldiasA <javier.saldias.arya@gmail.com>
2026-03-24 22:11:51 +0900

    cleaning

--- sirecor2.0/views/template/footer.php
+++
@@ -1,6 +0,0 @@
-
-<script src="<?= BASE_URL ?>assets/js/bootstrap.min.js"></script>
-<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-<script src="<?= BASE_URL?>assets/js/funtions.js"></script>
-</body>
-</html>
