e98e1523e502b230d4cbd2f9231610b0992ea149
JsaldiasA <javier.saldias.arya@gmail.com>
2026-03-26 01:49:28 +0900

    fix navbar

--- views/main.php
+++ views/main.php
@@ -7,19 +7,14 @@
    </script>
 </head>
 
+<div id="nav-placeholder"></div>
+<script>
+  $(function(){
+    $("#nav-placeholder").load("../views/navbar.html");
+  });
+</script>
 
 
-
-<?php
-
-
-$self=$_SERVER['PHP_SELF']; 
-$thispath=dirname($_SERVER['PHP_SELF']);
-$sitebasepath=$_SERVER['DOCUMENT_ROOT'];
-	
-require_once $sitebasepath.'/views/navbar.php';
-
-?>
 
 <script>
 
