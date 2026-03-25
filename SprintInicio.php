29f1f3f0eea133d857bc4b55af717ab510e0d060
JsaldiasA <javier.saldias.arya@gmail.com>
2026-03-25 23:27:03 +0900

    fix

--- SprintInicio.php
+++ SprintInicio.php
@@ -1,6 +1,10 @@
 <?php
 
-require_once 'views/page.php';
+$self = $_SERVER['PHP_SELF']; 
+$thispath = dirname($_SERVER['PHP_SELF']);
+$sitebasepath = $_SERVER['DOCUMENT_ROOT'];
+
+require_once $sitebasepath."views/page.php";
 
 $Page = new page();
 $Model = $Page->get_Model();
