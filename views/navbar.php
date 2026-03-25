89f7da8a50a167627905ae2e9982f274bd37b251
JsaldiasA <javier.saldias.arya@gmail.com>
2026-03-25 23:35:42 +0900

    fix3

--- views/navbar.php
+++ views/navbar.php
@@ -52,7 +52,7 @@ echo'
           <a class="nav-link" href="../ChecklistInicio.php" >Checklist</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="../SprintInicio.php" >Sprint</a>
+          <a class="nav-link" href="../views/SprintInicio.php" >Sprint</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
