d582541abd24c1deab7ef1bcb4e0a16ed4c4fc40
JsaldiasA <javier.saldias.arya@gmail.com>
2026-04-01 20:44:41 +0900

    agregar cuartel y zona

--- views/scripts/CuartelesVer.js
+++ views/scripts/CuartelesVer.js
@@ -63,7 +63,6 @@ async function GetMain(  )
 								{
 									HasRegistrio=true;
 
-
 									tableHTML +='<tr>';
 									tableHTML += `<td> ${row["Name"]}</td>`;
 									tableHTML += `<td> <a href='unidadver.php?tag=${UnidadTag}'>${UnidadSerie}</a></td>`;
@@ -129,6 +128,82 @@ async function GetMain(  )
 			tableHTML+= GetUnidadesTableById_unidadTipo( null ,'indefinidas',Cuarteles,Unidades,UltimosRegistros);
 
 			break;	
+		case 'sin cuartel':
+
+				tableHTML += '	<div class="row pb-3">';
+				tableHTML += ' <div class="col p-3 card shadow p-3 card shadow">';
+				tableHTML += `    <h2><b>Sin cuartel</b></h2> `;
+				tableHTML += '   <div class="overflow-auto">';
+					
+				tableHTML += '<table class="table text-nowrap"><thead><tr>';
+				tableHTML += `<th scope="col"><i class="bi bi-pin-map"></i></th>`;
+				tableHTML += `<th scope="col"><i class="bi bi-motherboard"></i></i></th>`;
+				tableHTML += `<th scope="col"></th>`;
+				tableHTML += `<th scope="col"><i class="bi bi-activity"></i></th>`;
+				tableHTML += `<th scope="col"><i class="bi bi-lightning-fill"></i></th>`;
+				tableHTML += `<th scope="col">[L/m]</th>`;
+				tableHTML += `<th scope="col">[L]</th>`;
+				tableHTML += `</thead>`;
+
+					Unidades.forEach(rowUnidades => {
+
+						let HasRegistrio = false;
+						let HasCuartel = false;
+						let UnidadSerie = '';
+						let UnidadTag = ''; 
+				
+						UnidadSerie = rowUnidades["Serie"];
+						UnidadTag = rowUnidades["tag"];
+
+						Cuarteles.forEach(rowCuarteles => {
+										
+							HasCuartel = (rowUnidades["Id"] == rowCuarteles["Id_unidad"] ) ? true : HasCuartel;
+							
+						});	
+						
+
+						if(!HasCuartel )
+						{	
+							UltimosRegistros.forEach(rowUr => {
+							
+								if(rowUnidades["Id"] == rowUr["unidad_id"])
+								{
+									HasRegistrio=true;
+
+									tableHTML +='<tr>';
+									tableHTML += `<td> ${rowUnidades["Serie"]}</td>`;
+									tableHTML += `<td> <a href='unidadver.php?tag=${UnidadTag}'>${UnidadSerie}</a></td>`;
+									tableHTML += `<td>${rowUr["ESTADO"]}</td>`;
+									tableHTML += `<td>${FieldActivity(rowUr["DATETIME"])}</td>`;
+									tableHTML += `<td>${FieldBattery(rowUr["VOLTAJE"])}</td>`;
+									tableHTML += `<td>${rowUr["CAUDAL"]}</td>`;
+									tableHTML += `<td>${rowUr["VOLUMEN"]}</td>`;
+									
+								}
+							})
+
+							if(!HasRegistrio)
+							{
+								tableHTML += '<tr >';
+								tableHTML += `<td> </td>`;
+								tableHTML += `<td> <a href='unidadver.php?tag=${UnidadTag}'>${UnidadSerie}</a></td>`;
+								tableHTML += `<td>Milesight</td>`;
+								tableHTML += `<td></td>`;
+								tableHTML += `<td></td>`;
+								tableHTML += `<td></td>`;
+								tableHTML += `<td></td>`;
+								tableHTML += '</tr>';
+							}
+						}			
+					});	
+
+				tableHTML += '</tbody></table>';
+				tableHTML += '          </div>';// div overflow
+				tableHTML += '    </div>      ';  // col
+				tableHTML += '</div>'; // row
+
+			break;
+		
 		default:
 			// Code to execute if no match is found
 		}
@@ -253,20 +328,19 @@ function GetUnidadesTableById_unidadTipo( unidadTipo ,Titulo ,Cuarteles,Unidades
 									}
 								})
 								if(!HasRegistrio)
-							{
-								tableHTML += '<tr >';
-								tableHTML += `<td> </td>`;
-								tableHTML += `<td> <a href='unidadver.php?tag=${UnidadTag}'>${UnidadSerie}</a></td>`;
-								tableHTML += `<td>Milesight</td>`;
-								tableHTML += `<td></td>`;
-								tableHTML += `<td></td>`;
-								tableHTML += `<td></td>`;
-								tableHTML += `<td></td>`;
-								tableHTML += '</tr>';
-							}
+								{
+									tableHTML += '<tr >';
+									tableHTML += `<td> </td>`;
+									tableHTML += `<td> <a href='unidadver.php?tag=${UnidadTag}'>${UnidadSerie}</a></td>`;
+									tableHTML += `<td>Milesight</td>`;
+									tableHTML += `<td></td>`;
+									tableHTML += `<td></td>`;
+									tableHTML += `<td></td>`;
+									tableHTML += `<td></td>`;
+									tableHTML += '</tr>';
+								}
 
-							}	
-					
+							}			
 						});	
 			
 			}
