cb3213a3b0f0ad5c1c111e0680af91f3b732ff81
JsaldiasA <javier.saldias.arya@gmail.com>
2026-06-24 01:00:58 +0900

    eventMessage

---
+++ ApiController/EventMessage/Delete.php
@@ -0,0 +1,20 @@
+<?php
+// Create SQL Connection
+
+header("Access-Control-Allow-Origin: *");
+
+$self=$_SERVER['PHP_SELF'];
+$thispath=dirname($_SERVER['PHP_SELF']);
+$sitebasepath=$_SERVER['DOCUMENT_ROOT'];
+require_once $sitebasepath."/Model/model.php";
+
+$model = new Model();
+$tableName = '';
+
+$Id_Obj= $_POST['Id'];
+$Obj= $model->MYSQLSelectWHERE($tableName ,'Id',$Id_Obj)[0];
+$result = $model->MYSQLDelete($tableName ,$Obj);
+
+echo 'alert( Eliminado exitosamente)';
+
+?>
