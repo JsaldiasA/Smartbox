65433f804fa18c074fcac7123e0fae1f4420d46e
JsaldiasA <javier.saldias.arya@gmail.com>
2026-06-24 00:07:15 +0900

    test4

--- test.php
+++ test.php
@@ -23,6 +23,24 @@ foreach($parameters as $p )
 	echo "Parametro: ".$p." =" . $DataJson->$p;
 }	
 
+date_default_timezone_set('America/Santiago');
+$FechaActual= date_create(date("Y-m-d H:i:s"));
+
+
+$NewObj = new eventmessageDbEntity();// use the name of the table related to the db entity
+
+
+$NewObj->Id = '0' ;
+$NewObj->MessageText = '{  "NewStatus": "BAJO", "LastStatus": "MEDIO"  }';
+$NewObj->CreationDate = $FechaActual;
+$NewObj->Id_MessageType = '1';
+$NewObj->Id_unidad		 = '983';
+
+
+// SET Default values
+
+$model->MYSQLInsertInto($eventmessage ,$NewObj);
+
 
 echo var_dump($parameters);
 
