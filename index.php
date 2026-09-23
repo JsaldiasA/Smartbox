031295effc32df3bea546470ce7e878f45a0b5b4
JsaldiasA <javier.saldias.arya@gmail.com>
2026-09-23 23:18:53 +0900

    index

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
 
-	
 	<!-- meta tags -->
 	<title>Eco3</title>
 	<link rel="shortcut icon" href="/favicon/favicon.jpg"/>
@@ -11,48 +10,37 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!-- stylesheets -->
-     
 	 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 	<link rel="stylesheet" href="https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.css">
 
 	<!-- scripts -->
-       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 	<script src="https://cdn.datatables.net/2.3.4/js/dataTables.js"></script>
-	
-	
-  
+
 </head>
 
 <div id="navbar"></div>
 
-<script src="src/navbar.js"></script>
   <body>
 
-    <div id="main" class="container"></div>
+		<div id="main" class="container"></div>
 
-    <script src="src/services.js"></script>
-	 <script src="src/Views/Page.js"></script>
-	  <script src="src/Views/TicketPage.js"></script>
-	  <script src="src/Views/RfvTicketPage.js"></script>
-	   <script src="src/Views/EventCenterTest.js"></script>
-	   <script src="src/Views/CuartelesPage.js"></script>
-	   <script src="src/Controllers/RfvTicketController.js"></script>
-	<script src="src/Model.js"></script>
-	<script src="src/App.js"></script>
-    <script src="src/CuartelesVer.js"></script>
-    <script src="src/ChecklistInicio.js"></script>
-    <script src="src/TicketInicio.js"></script>
-	<script src="src/Sprint.js"></script>
-	<script src="src/Login.js"></script>
-	<script src="src/EventCenter.js"></script>
-    <script type="module" src="/src/main.js"></script>
+		<script src="src/API.js"></script>
+		<script src="src/Views/Page.js"></script>
+		<script src="src/Views/TicketPage.js"></script>
+		<script src="src/Views/RfvTicketPage.js"></script>
+		<script src="src/Views/EventCenterPage.js"></script>
+		<script src="src/Views/CuartelesPage.js"></script>
+		<script src="src/Views/ChecklistPage.js"></script>
+		<script src="src/Controllers/RfvTicketController.js"></script>
+		<script src="src/Controllers/LoginController.js"></script>
+		<script src="src/Model.js"></script>
+		<script src="src/App.js"></script>
+		<script type="module" src="/src/main.js"></script>
 
-	 <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-	
+		<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+		
   </body>
 </html>
-
